55abc73e802284612d09b33c52945a6e788db5e5
Lucas <lucasmunizfk@gmail.com>
2025-02-15 20:58:23 -0300

    comment section created

---
+++ src/components/CardComment/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function CardComment(){
+    return(
+        <>
+            <div className="dsct-comment">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+            </div>
+        </>
+    );
+}
