792b8e96d52e1497d0f3635db8b07747d95584a1
Lucas <lucasmunizfk@gmail.com>
2025-02-15 20:24:54 -0300

    Header Component Created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="container">
+                <h1>DSCarrosTop</h1>
+            </nav>
+        </header>
+    );
+}
---
+++ src/routes/MainPage/index.tsx
@@ -0,0 +1,20 @@
+import Header from "../../components/Header";
+
+export default function MainPage(){
+    return(
+        <>
+            <Header />
+            <main>
+                <section id="car-catalog-section">
+                    <h2>Venha nos visitar</h2>
+                    <div className="dsct-card-container">
+
+                    </div>
+                </section>
+                <section id="comment-section">
+
+                </section>
+            </main>
+        </>
+    );
+}
