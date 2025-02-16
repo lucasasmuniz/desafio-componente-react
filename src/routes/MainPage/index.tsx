cbf3a512c532919b91f2ee6e3dadd868d3d03289
Lucas <lucasmunizfk@gmail.com>
2025-02-15 22:03:04 -0300

    last adjusts

--- src/routes/MainPage/index.tsx
+++ src/routes/MainPage/index.tsx
@@ -1,4 +1,8 @@
+import "./styles.css";
+import CardCar from "../../components/CardCar";
 import Header from "../../components/Header";
+import CardComment from "../../components/CardComment";
+import Footer from "../../components/Footer";
 
 export default function MainPage(){
     return(
@@ -6,14 +10,27 @@ export default function MainPage(){
             <Header />
             <main>
                 <section id="car-catalog-section">
-                    <h2>Venha nos visitar</h2>
-                    <div className="dsct-card-container">
-
+                    <div className="dsct-title-section">
+                        <h2>Venha nos visitar</h2>
+                    </div>
+                    <div className="dsct-card-container container">
+                        <CardCar />
+                        <CardCar />
                     </div>
                 </section>
                 <section id="comment-section">
-
+                    <div className="dsct-title-section">
+                        <h2 >O que estão dizendo</h2>
+                    </div>
+                    <div className="dsct-comment-container container">
+                        <CardComment />
+                        <CardComment />
+                        <CardComment />
+                        <CardComment />
+                        <CardComment />
+                    </div>
                 </section>
+                <Footer />
             </main>
         </>
     );
