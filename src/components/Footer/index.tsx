b4bcfeb12bd0d3f2f43e6a501e2bfb08f4973aa5
Lucas <lucasmunizfk@gmail.com>
2025-02-15 22:02:55 -0300

    Footer component created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function Footer(){
+    return(
+        <>
+            <footer>
+                <div className="dsct-footer-container container">
+                    <a href="">@dscarrostop</a>
+                    <p>Rua da Pipoca, 150</p>
+                </div>
+            </footer>
+        </>
+    );
+}
